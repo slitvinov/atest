b9d3d3784b18e2c237aa7972395eb3ca713a59d3
Sergey Litvinov <slitvinov@gmail.com>
2017-04-15 18:24:04 +0200

    minor

--- atest.example.sh
+++ atest.example.sh
@@ -19,10 +19,11 @@
 # TEST: r4
 # ./atest.example.sh | awk '{print $1*$2*$3}' > run.out.vtk
 #
-# TEST: r5
+# numeric test
+# nTEST: r5
 # ./atest.example.sh > run.out.vtk
 #
-# shoud fail
+# should fail
 # nTEST: r6
 # echo 1 2 3 4 5 > run.out.vtk
 
